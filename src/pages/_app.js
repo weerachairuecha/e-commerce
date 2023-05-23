4c87003f7605847a87a4d5447dbea920f4ec67a0
weerachai3233@gmail.com <weerachai3233@gmail.com>
2023-05-23 21:47:52 +0700

    update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,11 +6,22 @@ import "@fontsource/roboto/700.css";
 
 import { ThemeProvider } from "@mui/material";
 import theme from "@/theme";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kanit:wght@200;300;400;500&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,13 +1,142 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Box,
+  Stack,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+  Button,
+  TableContainer,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Collapse,
+  IconButton,
+} from "@mui/material";
 import Header from "@/components/header";
 import Content from "@/components/content";
 import Footer from "@/components/footer";
 import PageLayout from "@/components/page-layout";
+import { Add, Remove } from "@mui/icons-material";
 
 const CartPage = () => {
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [cartList, setCartList] = useState([
+    {
+      id: 1,
+      name: "name",
+      amount: 2,
+      price: 20,
+    },
+    {
+      id: 2,
+      name: "name2",
+      amount: 4,
+      price: 50,
+    },
+  ]);
+
+  const handleCheckoutButton = () => {};
   return (
-    <PageLayout>Cart Page</PageLayout>
+    <PageLayout>
+      <Content>
+        <Divider sx={{ fontSize: "20px" }}>ตะกร้า</Divider>
+        <List sx={{ padding: 0 }}>
+          {cartList.map((item, index) => {
+            return (
+              <ProductItem
+                key={index}
+                name={item.name}
+                amount={item.amount}
+                price={item.price}
+                total={item.price * item.amount}
+              />
+            );
+          })}
+        </List>
+
+        <Stack alignItems={"flex-end"}>
+          <Button
+            variant={"contained"}
+            sx={{
+              marginTop: "20px",
+            }}
+            onClick={handleCheckoutButton}
+          >
+            ชำระเงิน
+          </Button>
+        </Stack>
+      </Content>
+    </PageLayout>
+  );
+};
+
+const ProductItem = ({ name, amount, price, total }) => {
+  const [openCollapse, setOpenCollapse] = useState(false);
+  const toggleCollapse = () => setOpenCollapse(!openCollapse);
+
+  return (
+    <>
+      <ListItem
+        sx={{ padding: 0, paddingY: "10px", cursor: "pointer" }}
+        onClick={toggleCollapse}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+          }}
+        >
+          <Box
+            sx={{
+              width: "50px",
+              height: "30px",
+              border: "2px solid",
+              borderColor: (theme) => theme.palette.primary.main,
+              color: (theme) => theme.palette.primary.main,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              borderRadius: "6px",
+            }}
+          >
+            {amount}x
+          </Box>
+          <Typography sx={{ marginLeft: "4px", flex: 1 }} noWrap>
+            {name}
+          </Typography>
+          <Typography sx={{ width: "80px", textAlign: "right" }}>
+            {price}
+          </Typography>
+          <Typography sx={{ width: "80px", textAlign: "right" }}>
+            {total}
+          </Typography>
+        </Box>
+      </ListItem>
+      <Collapse in={openCollapse}>
+        <Stack
+          direction={"row"}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <IconButton>
+            <Remove />
+          </IconButton>
+          <Typography sx={{ width: "100px", textAlign: "center" }}>
+            {amount}
+          </Typography>
+          <IconButton>
+            <Add />
+          </IconButton>
+        </Stack>
+      </Collapse>
+      <Divider />
+    </>
   );
 };
 
